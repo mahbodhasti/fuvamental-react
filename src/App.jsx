b43ca428c2bb215ea7924e1cba449aafeac4ed5b
mahbodhasti <84708375+mahbodhasti@users.noreply.github.com>
2025-03-21 21:57:16 +0330

    fetchUseffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,18 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Contact from "./component/Contact";
-import Footer from "./component/Footer";
-import Header from "./component/Header";
-import HomePage from './pages/HomePage';
-import AboutPage from "./pages/AboutPage";
-import ContactPage from "./pages/ContactPage";
-import NotFound from "./pages/NotFound";
-import PersonFaviver from "./pages/PersonFaviver";
+import React, { useEffect, useState } from 'react';
 
+const App = () => {
+   let [data,setData]= useState()   
+    useEffect( ()=>{
+       fetch('https://dummyjson.com/products')
+        .then(res => res.json())
+        .then(json=>setData(json))
+       
+    })
+    return (
+        <div>
+            {JSON.stringify(data)}
+        </div>
+    );
+};
 
-
-const App   = () => {
- 
-  
-  
-  return (
-    <div> 
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<HomePage/>} />
-          <Route path='/about/:id/:name' element={<AboutPage/>} />
-          <Route path='/contact' element={<ContactPage/>} />
-          <Route path='404' element={<NotFound/>} />
-          <Route path='/favritPrson/:person/:music/:rate' element={<PersonFaviver/>} />
-
-        </Routes>
-      </BrowserRouter>
-      
-    </div> 
-
-  )
-}
-
-
-export default App  ;
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import "./assets/css/style.css"
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'milligram/dist/milligram.css'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+
     <App/>
-  </StrictMode>,
+
 )
